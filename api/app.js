527bea56ec4d2a068ff9fd641c93f96a89a4f502
Quy Tran <tuanquynet@gmail.com>
2019-05-11 12:23:50 +0700

    add package.json, mongodb connection to mongodb cloud

--- api/app.js
+++ api/app.js
@@ -16,9 +16,10 @@ app.use(cors());
 
 // connect mongodb
 const MONGO_URI = 'mongodb://localhost:27017/todo';
+const MONGO_URI = 'mongodb+srv://devops:share123@cluster0-5kuqc.mongodb.net/todo?retryWrites=true';
 
 mongoose.connect(MONGO_URI, { useNewUrlParser: true }, () => {
-	console.log('Connect to MongoDB @', MONGO_URI, ' successfully.');
+    console.log('Connect to MongoDB @', MONGO_URI, ' successfully.');
 });
 
 // view engine setup
@@ -37,18 +38,18 @@ app.use('/tasks', tasksRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
